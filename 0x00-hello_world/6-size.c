ddc13084a253df7401a175a2879afb76f82bdbec
Jdpa357 <jdpa352@hotmail.es>
2019-09-18 19:03:00 +0000

    Fixing errors of betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-  char A = 'A';
-  int B = 2;
-  long int C = 34;
-  long long int D = 9223372036;
-  float E = 2.1416;
+char A = 'A';
+int B = 2;
+long int C = 34;
+long long int D = 9223372036;
+float E = 2.1416;
 
-  printf("Size of a char: %d byte(s) \n", sizeof(A));
-  printf("Size of a int: %d byte(s) \n", sizeof(B));
-  printf("Size of a long int: %d byte(s) \n", sizeof(C));
-  printf("Size of a long long: %d byte(s) \n", sizeof(D));
-  printf("Size of a float: %d byte(s) \n", sizeof(E));
-  return(0);
+printf("Size of a char: %d byte(s) \n", sizeof(A));
+printf("Size of a int: %d byte(s) \n", sizeof(B));
+printf("Size of a long int: %d byte(s) \n", sizeof(C));
+printf("Size of a long long: %d byte(s) \n", sizeof(D));
+printf("Size of a float: %d byte(s) \n", sizeof(E));
+return (0);
 };
