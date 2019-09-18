792ba2e19f700b0440441894aab83d5ca65ab244
Jdpa357 <jdpa352@hotmail.es>
2019-09-18 19:06:11 +0000

    Correcting strings

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ long long int D = 9223372036;
 float E = 2.1416;
 
 printf("Size of a char: %d byte(s) \n", sizeof(A));
-printf("Size of a int: %d byte(s) \n", sizeof(B));
+printf("Size of an int: %d byte(s) \n", sizeof(B));
 printf("Size of a long int: %d byte(s) \n", sizeof(C));
 printf("Size of a long long int: %d byte(s) \n", sizeof(D));
 printf("Size of a float: %d byte(s) \n", sizeof(E));
