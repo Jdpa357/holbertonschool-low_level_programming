7917830250fc197f93ff843643eddb8d80e2007a
Jdpa357 <jdpa352@hotmail.es>
2019-09-18 19:04:36 +0000

    Adding string

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ float E = 2.1416;
 printf("Size of a char: %d byte(s) \n", sizeof(A));
 printf("Size of a int: %d byte(s) \n", sizeof(B));
 printf("Size of a long int: %d byte(s) \n", sizeof(C));
-printf("Size of a long long: %d byte(s) \n", sizeof(D));
+printf("Size of a long long int: %d byte(s) \n", sizeof(D));
 printf("Size of a float: %d byte(s) \n", sizeof(E));
 return (0);
 };
