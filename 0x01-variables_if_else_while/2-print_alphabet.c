250ae987aab0b831ae39aef7a8051e7175f3dfeb
Jdpa357 <jdpa352@hotmail.es>
2019-09-19 19:19:44 +0000

    Adding comments

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,6 +2,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
   char alpha;
