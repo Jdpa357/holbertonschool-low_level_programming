4b793770cb2983ac47260d73c5fc9ec127f59fa4
Jdpa357 <jdpa352@hotmail.es>
2019-09-19 19:21:48 +0000

    Correcting according to betty

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,11 +9,12 @@
  */
 int main(void)
 {
-  char alpha;
+	char alpha;
 
-  for(alpha = 'a'; alpha <= 'z'; alpha++){
-    putchar(alpha);
-  }
-  putchar('\n');
-  return (0);
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
 }
