e7599ec6b10b34a37c0a83cdaf648d3daf1991f0
Jdpa357 <jdpa352@hotmail.es>
2019-09-19 19:29:15 +0000

    Last mandatory exercise

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/*Headers*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int i;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			putchar(n);
+			putchar(i);
+			if (n < '9' || i < '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
