b68a86048979d407919497a963c790ba77f3a2c5
Jdpa357 <jdpa352@hotmail.es>
2019-09-24 15:00:40 +0000

    Adding some variables to control the return

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,17 +6,22 @@
  */
 int print_sign(int n)
 {
+	int result;
+
 	if (n > 0)
 	{
-		_putchar("+");
-		return (1);
+		result = 1;
+		_putchar('+');
 	}
 	else if (n < 0)
 	{
-		_putchar("-");
-		return (-1);
+		result = -1;
+		_putchar('-');
 	}
 	else
-		_putchar("0");
-		return (0);
+	{
+		result = 0;
+		_putchar('0');
+	}
+	return (result);
 }
