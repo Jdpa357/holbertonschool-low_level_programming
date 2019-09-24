9771828d687831efbf60d399fe1d3642ab015b0f
Jdpa357 <jdpa352@hotmail.es>
2019-09-24 14:34:32 +0000

    Deleting some unuseful variables

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,13 @@
 void print_alphabet_x10(void)
 {
 	char alpha;
-	int result, i;
+	int  i;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
-			result += result + alpha;
 		}
 	_putchar('\n');
 	}
