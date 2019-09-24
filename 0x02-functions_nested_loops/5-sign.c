752bf13e5bad3e8790fa2a5e3a40553ceb6b7078
Jdpa357 <jdpa352@hotmail.es>
2019-09-24 14:58:26 +0000

    Correcting comments and indentation

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,21 +1,22 @@
 #include "holberton.h"
-/*
+/**
  * print_sign - Prints the sign of a number
+ * @n: Character passed to function in order to be evaluated
  * Return: 1 if is greater than zero, 0 if its zero, -1 if its less than zero
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar("+");
-	return(1);
+		_putchar("+");
+		return (1);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
-	_putchar("-");
-	return(-1);
+		_putchar("-");
+		return (-1);
 	}
 	else
-	_putchar("0");
-	return(0);
+		_putchar("0");
+		return (0);
 }
