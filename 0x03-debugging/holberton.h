cf629f7d64fe30235fb25d2f40c8a08d8b160ed6
Jdpa357 <jdpa352@hotmail.es>
2019-09-24 16:39:45 +0000

    Adding new exercises to repository

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -2,3 +2,6 @@
  * positive_or_negative - function for the first task of project
  */
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
