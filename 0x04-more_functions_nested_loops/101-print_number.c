68dd245ecfe51ecbdfe076b4d88006a8d649a38f
Jdpa357 <jdpa352@hotmail.es>
2019-09-26 19:59:18 +0000

    Last advanced exercise

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_number - Function that prints an integer char by char
+ * @n: Integer to be used by the function
+ * Return: No return (Void function)
+ */
+
+void print_number(int n)
+{
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n/10)
+	{
+		print_number(n/10);
+	}
+	_putchar(n%10 + '0');
+
+}
