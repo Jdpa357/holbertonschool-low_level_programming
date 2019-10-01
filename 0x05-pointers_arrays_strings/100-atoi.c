b209214a07a63d3dcbc4f72dbfa2307cd9f58169
Jdpa357 <jdpa352@hotmail.es>
2019-10-01 21:08:05 +0000

    First advanced exercise

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+/**
+ * _atoi - Function that converts a string to an integer
+ * @s: Parameter passed to the function
+ * Return: Integer value on the string
+ */
+
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	for (; s[i] != '\0'; i++)
+	{
+		result = result * 10 + s[i] - '0';
+	}
+	return (sign * result);
+
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -17,5 +17,6 @@ void puts2(char *);
 void puts_half(char *);
 void print_array(int *, int);
 char *_strcpy(char *, char *);
+int _atoi(char *);
 
 #endif /* HOLBERTON_H */
