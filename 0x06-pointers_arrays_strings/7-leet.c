8411b671204291356b175e9d6dba9722edd9df47
Jdpa357 <jdpa352@hotmail.es>
2019-10-03 20:42:43 +0000

    Adding 7th exercise

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * leet - Function to encode a string
+ * @a: Parameter passed to the function to process
+ * Return: Encoded string
+ */
+
+char *leet(char *a)
+{
+	int i, j;
+	char letter[] = "a,A,e,E,o,O,t,T,l,L";
+	char coding[] = "4,4,3,3,0,0,7,7,1,1";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; letter[j] != '\0';j++)
+		{
+			if (a[i] == letter[j])
+			{
+				a[i] = coding[j];
+			}
+		}
+	}
+
+	return (a);
+
+}
