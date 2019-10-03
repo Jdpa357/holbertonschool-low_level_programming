84978a67505b072f0e3297219e3ccdef12913186
Jdpa357 <jdpa352@hotmail.es>
2019-10-03 19:04:54 +0000

    Adding sixth exercise to project

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+/**
+ * cap_string - Function to capitalize all words of a string
+ * @a: Parameter to be processed by the function
+ * Return: Pointer with all the string capitalized
+ */
+
+char *cap_string(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] !='\0'; i++)
+	{
+		if (i == 0)
+		{
+			if(a[i] >= 'a' && a[i] <= 'z')
+			{
+				a[i] = a[i] - 32;
+			}
+		}
+		if (a[i] == ' ' || a[i] == '\t' || a[i] == '\n' || a[i] == ',' || a[i] == ';' || a[i] == '.' || a[i] == '!' || a[i] == '?' || a[i] == '"' || a[i] == '(' || a[i] == ')' || a[i] == '{' || a[i] == '}')
+		{
+			++i;
+			if (a[i] == ' ')
+			{
+				++i;
+			}
+			if (a[i] >= 'a' && a[i] <= 'z')
+			{
+				a[i] = a[i] - 32;
+			}
+		}
+		else
+		{
+			if(i != 0)
+			{
+				if (a[i] >= 'A' && a[i] <= 'Z')
+				{
+					a[i] = a[i] + 32;
+				}
+			}
+		}
+	}
+
+	return (a);
+
+}
