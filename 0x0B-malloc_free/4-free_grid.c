c6dbf37067ee67f7ced476ba3be94f3cdb02e5a2
Jdpa357 <jdpa352@hotmail.es>
2019-10-15 19:23:27 +0000

    Adding comments to description of function

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "holberton.h"
 
-/*
+/**
  * free_grid - Function to free a 2 dimensional grid previously created
  * @grid: First parameter passed to function to process
  * @height: Second parameter passed to function to process
