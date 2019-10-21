7f5eb65dd6eaa506fc7eaedf32e5fc2d0deb8324
Jdpa357 <jdpa352@hotmail.es>
2019-10-21 23:09:40 +0000

    Adding last exercise of project to repository

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - Function to create a new structure type defined on header file
+ * "dog.h"
+ * @name: Attribute of the structure type
+ * @age: Attribute of the structure type
+ * @owner: Attribute of the structure type
+ * Return: New structure type defined on header file "dog.h"
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	dot_t *doggie;
+	int i, j, nameLength, ownerLength;
+
+	doggie = malloc(sizeof(struct dog));
+	if (doggie == NULL)
+		return (NULL);
+	for (nameLength = 1, name[nameLength] != '\0'; nameLength++)
+		;
+	doggie->name = malloc(sizeof(char) * (nameLength + 1));
+	if (doggie->name == NULL)
+	{
+		free(doggie);
+		return (NULL);
+	}
+	for (ownerLength = 1; owner[ownerLength] != '\0'; ownerLength++)
+		;
+	doggie->owner = malloc(sizeof(char) * (ownerLength + 1));
+	if (doggie->owner == NULL)
+	{
+		free(doggie->name);
+		free(doggie->owner);
+		return (NULL);
+	}
+	doggie->age = age;
+	for (i = 0; name[i] != '\0'; i++)
+		doggie->name = name[i];
+	doggie->name = '\0';
+	for (j = 0; owner[j] != '\0'; j++)
+		doggie->owner = owner[j];
+	doggie->owner = '\0';
+
+	return (doggie);
+
+}
