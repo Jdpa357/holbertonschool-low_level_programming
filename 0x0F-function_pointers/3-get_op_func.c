a835ee4690ef5ac4e296efc56a7aac0489d45750
Jdpa357 <jdpa352@hotmail.es>
2019-10-23 17:41:59 +0000

    Fixing betty style

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,22 +2,23 @@
 #include "3-calc.h"
 
 /**
- *
- *
- *
- *
+ * get_op_func - Function that selects the correct function to perform an
+ * operation asked by the user
+ * @s: First parameter passed to function
+ * Return: A pointer to the function that corresponds to the operator given
+ * as parameter
  */
 
 int (*get_op_func(char *s))(int, int)
 {
 
 	op_t ops[] = {
-        	{"+", op_add},
-        	{"-", op_sub},
-        	{"*", op_mul},
-        	{"/", op_div},
-        	{"%", op_mod},
-        	{NULL, NULL}
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 	int i = 0;
 
