04b7370e67fbf24d865dc780750ce6f5ceecffc1
Jdpa357 <jdpa352@hotmail.es>
2019-10-27 14:02:20 +0000

    Adding variable names to header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,12 +3,12 @@
 
 typedef struct args_t
 {
-  char *format;
-  void (*function)();
+	char *format;
+	void (*function)();
 } args_t;
 
 int _putchar(char);
-int sum_them_all(const unsigned int, ...);
+int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *, const unsigned int, ...);
 void print_strings(const char *, const unsigned int, ...);
 void print_all(const char * const, ...);
