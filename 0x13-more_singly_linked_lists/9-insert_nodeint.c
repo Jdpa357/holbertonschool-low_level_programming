f3710888bb74c01a0138fa4fd668f7ae212eba34
Jdpa357 <jdpa352@hotmail.es>
2019-11-05 12:52:33 +0000

    Adding new statement for failure

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (i = 0; i < (idx - 1); i++)
 	{
 		current = current->next;
+		if (current->next == NULL)
+		{
+			return (NULL);
+		}
 	}
 	newest->next = current->next;
 	current->next = newest;
