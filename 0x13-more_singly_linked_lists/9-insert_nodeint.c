ce225cd5b816d47287fc5fefe50aacf7fc38c393
Jdpa357 <jdpa352@hotmail.es>
2019-11-05 13:17:03 +0000

    Adding free statement

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,14 +17,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *current = *head;
 	unsigned int i;
 
-	if (newest == NULL || *head == NULL)
+	if (newest == NULL || head == NULL)
 		return (NULL);
 	newest->n = n;
 	if (idx == 0)
 	{
-		newest->next = current->next;
+		newest->next = *head;
 		*head = newest;
-		return (*head);
+		return (newest);
 	}
 	else
 	{
@@ -33,6 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			current = current->next;
 			if (current->next == NULL)
 			{
+				free(newest);
 				return (NULL);
 			}
 		}
